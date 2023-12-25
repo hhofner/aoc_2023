37446d6ba7384e47f7f394b3210bd96d2236a638
Hans Hofner <hhofner@gmail.com>
2023-12-25 11:31:54 +0900

    day 2

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,5 +1,56 @@
 defmodule AdventOfCode.Day02 do
-  def part1(_args) do
+  def part1(args) do
+    max = %{blue: 14, red: 12, green: 13}
+
+    args
+    |> Enum.map(fn x ->
+      IO.inspect(parse_line(x))
+    end)
+
+    args
+    |> Enum.reduce(0, fn x, acc ->
+      %{blue: blue_amount, red: red_amount, green: green_amount, id: id} = parse_line(x)
+
+      if blue_amount <= max[:blue] and red_amount <= max[:red] and green_amount <= max[:green] do
+        String.to_integer(id) + acc
+      else
+        acc
+      end
+    end)
+  end
+
+  defp parse_line(game_string) do
+    "Game " <> id = Enum.at(String.split(game_string, ":"), 0)
+
+    game_string
+    |> String.split(":")
+    |> Enum.at(1)
+    |> String.split(";")
+    |> Enum.reduce(%{id: id, red: 0, green: 0, blue: 0}, fn x, acc ->
+      x
+      |> String.split(",")
+      |> Enum.reduce(acc, fn y, acc ->
+        {color, amount} = parse_color_amount(y)
+
+        if String.to_integer(amount) > acc[color] do
+          Map.merge(acc, %{color => String.to_integer(amount)})
+        else
+          acc
+        end
+      end)
+    end)
+  end
+
+  defp parse_color_amount(input) do
+    IO.inspect(String.split(input, " "))
+    [amount, color] = String.split(String.trim(input), " ")
+
+    case color do
+      "green" -> {:green, amount}
+      "red" -> {:red, amount}
+      "blue" -> {:blue, amount}
+      _ -> {:error, "Invalid color"}
+    end
   end
 
   def part2(_args) do
